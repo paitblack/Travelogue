583b9511cf519b299fdbd69715664242126a1abc
U210709005 <emre-parlak2002@hotmail.com>
2026-01-04 02:02:53 +0300

    firestore sorunu düzeldi

--- app/src/main/java/msku/ceng/travelogue/Travelogue.java
+++ app/src/main/java/msku/ceng/travelogue/Travelogue.java
@@ -6,12 +6,30 @@ import android.content.SharedPreferences;
 
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+
 // SULEYMAN EMRE PARLAK
 public class Travelogue extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        FirebaseApp.initializeApp(/*context=*/ this);
+
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+
+        if (BuildConfig.DEBUG) {
+            firebaseAppCheck.installAppCheckProviderFactory(
+                    DebugAppCheckProviderFactory.getInstance()
+            );
+        } else {
+            firebaseAppCheck.installAppCheckProviderFactory(
+                    PlayIntegrityAppCheckProviderFactory.getInstance()
+            );
+        }
 
         SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
 
